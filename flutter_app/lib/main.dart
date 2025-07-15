c0a4bd5914fe29c866638b90ad37ecda51351212
Boe Zhou <55188595+boezzz@users.noreply.github.com>
2025-07-15 10:56:00 -0700

    basic Frame connection

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,125 +1,136 @@
+import 'dart:async';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
+import 'package:simple_frame_app/frame_vision_app.dart';
+import 'package:simple_frame_app/simple_frame_app.dart';
+import 'package:frame_msg/tx/plain_text.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MainApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+final _log = Logger("MainApp");
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  MainAppState createState() => MainAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class MainAppState extends State<MainApp> with SimpleFrameAppState, FrameVisionAppState {
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  // the image and metadata to show
+  Image? _image;
+  ImageMetadata? _imageMeta;
+  bool _processing = false;
+
+  MainAppState() {
+    Logger.root.level = Level.FINE;
+    Logger.root.onRecord.listen((record) {
+      debugPrint('${record.level.name}: ${record.time}: ${record.message}');
     });
   }
 
   @override
+  void initState() {
+    super.initState();
+    // kick off the connection to Frame and start the app if possible
+    tryScanAndConnectAndStart(andRun: true);
+  }
+
+  @override
+  Future<void> onRun() async {
+    final plainText = TxPlainText(text: '3-Tap: take photo');
+    await frame!.sendMessage(0x0a, plainText.pack());
+  }
+
+  @override
+  Future<void> onCancel() async {
+    // app-specific cleanup
+  }
+
+  @override
+  Future<void> onTap(int taps) async {
+    switch (taps) {
+      case 1:
+        // next
+        break;
+      case 2:
+        // prev
+        break;
+      case 3:
+        // check if there's processing in progress already and drop the request if so
+        if (!_processing) {
+          _processing = true;
+          // start new vision capture
+          // asynchronously kick off the capture/processing pipeline
+          capture().then(process);
+        }
+        break;
+      default:
+    }
+  }
+
+  /// The vision pipeline to run when a photo is captured
+  FutureOr<void> process((Uint8List, ImageMetadata) photo) async {
+    var imageData = photo.$1;
+    var meta = photo.$2;
+
+    try {
+      // NOTE: Frame camera is rotated 90 degrees clockwise, so by default RxPhoto makes it upright (`img.copyRotate()`) for image processing.
+      // Some processing packages e.g. ML Kit allow us to pass in a rotation parameter
+      // To save processing we can set `upright=false` when we construct/initialize our main FrameVisionApp class and handle it manually.
+
+      // update Widget UI
+      // For the widget we rotate it upon display with a transform, not changing the source image
+      Image im = Image.memory(imageData, gaplessPlayback: true,);
+
+      setState(() {
+        _image = im;
+        _imageMeta = meta;
+      });
+
+      // TODO Perform vision processing pipeline on the current image
+
+      // indicate that we're done processing
+      _processing = false;
+
+    } catch (e) {
+      _log.severe('Error processing photo: $e');
+      // TODO rethrow;?
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return MaterialApp(
+      title: 'Frame Vision',
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Frame Vision'),
+          actions: [getBatteryWidget()]
+        ),
+        drawer: getCameraDrawer(),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                children: [
+                  _image ?? Container(),
+                  const Divider(),
+                  if (_imageMeta != null) ImageMetadataWidget(meta: _imageMeta!),
+                ],
+              )
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            const Divider(),
           ],
         ),
+        floatingActionButton: getFloatingActionButtonWidget(const Icon(Icons.camera_alt), const Icon(Icons.cancel)),
+        persistentFooterButtons: getFooterButtonsWidget(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
